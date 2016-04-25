ea236e7f18699f28fa4149659d4baca3df7c7a3a
Honglin Zhang <zhang.t.honglin@gmail.com>
2016-04-24 20:25:49 -0700

    Add keybinding for dmenu; Migrate from Gnome to Xfce.

--- xmonad.hs
+++ xmonad.hs
@@ -1,10 +1,14 @@
 import XMonad
-import XMonad.Config.Gnome
+import XMonad.Config.Xfce
+import XMonad.Util.EZConfig
 
-main = xmonad gnomeConfig
-       {
-         modMask = mod4Mask
+main = xmonad $ xfceConfig
+       { terminal = "urxvt"
+       , modMask = mod4Mask
        , borderWidth = 2
        , focusedBorderColor = "#1C86EE"
        , normalBorderColor = "#1B1D1E"
        }
+       `additionalKeysP`
+       [ ("M-p", spawn "dmenu_run")
+       ]
